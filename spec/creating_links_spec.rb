fb9aa4b3948415d425170c7accdee0e25cd7d02e
Abi Travers <atravers4007@gmail.com>
2017-08-24 09:06:14 +0100

    Correcting spec test for adding form elements to list of links

--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -1,11 +1,17 @@
 feature 'submitting new links' do
 
   scenario 'I can use a form to submit a new link' do
-     within link_adding_from do
-       fill_in "Title", :with => "Hats of Meat"
-       fill_in "Url", :with => "www.hatsofmeat.com"
+     visit '/links/new'
+    #  within link_adding_from do
+       fill_in "title", :with => "Hats of Meat"
+       fill_in "url", :with => "www.hatsofmeat.com"
      click_button "Submit"
+
+     #expect to be redirected back to links page
+     expect(current_path).to eq '/links'
+
+     within 'ul#links' do
+       expect(page).to have_content("Hats of Meat")
      end
   end
-
 end
